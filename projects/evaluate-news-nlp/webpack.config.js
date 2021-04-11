8cf6efb7baf2052bebc85e669fc1f5cfbe4e75e8
RoseanneHealy <roseannehealy333@gmail.com>
2021-04-11 19:46:59 +0100

    loader/plugins

--- projects/evaluate-news-nlp/webpack.config.js
+++ projects/evaluate-news-nlp/webpack.config.js
@@ -1,6 +1,25 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 const path = require("path")
 const webpack = require("webpack")
 module.exports = {
     mode: 'development',
     entry: './src/client/index.js',
+module: {
+    rules: [
+            {
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: "babel-loader"
+            }
+    ]
+},
+plugins: [
+    new HtmlWebPackPlugin({
+        template: "./src/client/views/index.html",
+        filename: "./index.html",
+    }
+
+    )
+]
+
 }
