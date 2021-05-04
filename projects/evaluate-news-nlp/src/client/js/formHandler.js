3aa811614f62963aa5ee18760384fb7a2ae57262
RoseanneHealy <roseannehealy333@gmail.com>
2021-05-04 13:19:20 +0100

    changes 4/5

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,43 +1,18 @@
-function handleSubmit(event) {
-    event.preventDefault()
+export function handleSubmit(event) {
+  event.preventDefault()
+  let urlInput = document.getElementById('url').value
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
-
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => {
-        return res.json()
-    })
-    .then(function(data) {
-        document.getElementById('results').innerHTML = data.message
-    })
-}
-export { handleSubmit }
-
-//start of api// mentor: 28/04/21 Once you have the text, you have to make a POST request to the server(your own server) - server to use the fetch function. 
-
-  geturlInfo(e)
-    .then(
-      function (data) {        
-        //posting Data to server
-        postData('/addNewURL', { url: data.url})
-          .then(() => {
-            updateUI();
-        });
+  let input = {
+    url: encodeURI(urlInput)
+  }
+  console.log(input);
+  postData('/addNewURL', input);
+};
 //using the then keyword we can post our data to server .so we post data and use fetch later to receive it back. 
 //to have it appear on page, we need to dynamically update UI to have it on our static webpage.
 
-
-
-//async function (async so our code waits to receive back data from server.) 
-const geturlInfo = async (url) => {
-  const res = await fetch(url + /.env/apiKey);
-  
-
 const postData = async (url = '', data = {}) => {
-  const response = await fetch(url, {
+  const response = await fetch('http://localhost:8081' + url, {
     method: 'POST',
     credentials: 'same-origin',
     headers: {
@@ -48,7 +23,7 @@ const postData = async (url = '', data = {}) => {
 
   try {
     const newData = await response.json();
-    return newData;
+    updateUI(newData);
   } catch (error) {
     console.log("error", error);
   }
@@ -56,13 +31,11 @@ const postData = async (url = '', data = {}) => {
 
 //28/04/2021 Then on the client side, once you receive this data, extract the needed fields (they are shown in the project rubric) and call a function to update your UI (you can call it updateUI) by passing this data. 
 
-const updateUI = async () => {
-  const request = await fetch('/all')
-  
-    document.getElementById("model").innerHTML = `<p> Model: ${data.model}</p>`;
-    document.getElementById("score_tag").innerHTML = `<p> Score tag: ${data.score_tag}</p>`;
-    document.getElementById("agreement").innerHTML = `<p> Agreement: ${data.agreement}</p>`;
-    document.getElementById("subjectivity").innerHTML = `<p> Subjectivity: ${data.subjectivity}</p>`;
-    document.getElementById("confidence").innerHTML = `<p> Confidence: ${data.confidence}</p>`;
-    document.getElementById("irony").innerHTML = `<p> Irony: ${data.irony}</p>`;
-
+const updateUI = async (data) => {
+  document.getElementById("model").innerHTML = `<p> Model: ${data.model}</p>`;
+  document.getElementById("score_tag").innerHTML = `<p> Score tag: ${data.score_tag}</p>`;
+  document.getElementById("agreement").innerHTML = `<p> Agreement: ${data.agreement}</p>`;
+  document.getElementById("subjectivity").innerHTML = `<p> Subjectivity: ${data.subjectivity}</p>`;
+  document.getElementById("confidence").innerHTML = `<p> Confidence: ${data.confidence}</p>`;
+  document.getElementById("irony").innerHTML = `<p> Irony: ${data.irony}</p>`;
+}
--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,3 +1,6 @@
+//// Setup empty JS object to act as endpoint for all routes// do i need this? ...
+//let projectData = {};
+
 const dotenv = require('dotenv');
 dotenv.config();
 var path = require('path')
@@ -33,34 +36,25 @@ function listening() {
 
 const app = express()
 
-// Setup empty JS object to act as endpoint for all routes// do i need this? ...
-let projectData = {};
-
-app.get('/all', getData)
-
-function getData(req, res) {
-  res.send(projectData)
-}
+app.get('/', (req, res) => res.sendFile(path.resolve('dist/index.html')));
 
 //POST ROUTE (posting new info back) 28/04/2021  In this POST route's handler, you have to get the data sent from the form from the request object and make a GET request to MeaningCloud's API.
 app.use(express.static('dist'))
 
-app.post('/addNewURL')
+app.post('/addNewURL',async(req, res)=>{ 
+const meaningCloudTranslation = await fetch(`https://api.meaningcloud.com/sentiment-2.1?key=${key}&url=${url}&lang=en`, { method: 'POST' })
+});
 
-app.get('/', (req, res) => res.sendFile(path.resolve('dist/index.html')));
 
-//fetch here
-
-const key = 'API_KEY=2e4344a209be1bbe0f4124ad76d6683f'
-const url = //what to put here??
-
-fetch(`https://api.meaningcloud.com/sentiment-2.1?key=${key}&url=${url}&lang=en`, { method: 'POST' })
 
 //Once you get the data from the MeaningCloud API, simply send it back to the client.
 
-
-  .then(response => response.json())
-  .then(data => console.log(data));
-
-
+try{
+  const Completedata = await meaningCloudTranslation.json();
+  console.log(meaningCloudTranslation, Completedata)
+  res.send(Completedata);
+}catch(error){
+  console.log("error", error);
+}
 //end of api//
+
